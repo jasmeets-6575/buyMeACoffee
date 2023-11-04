e46cde04c457377e05b57cb4066d5bf21e89fd16
Jasmeet Singh <jasmeetsonu990@gmail.com>
2023-11-04 12:03:07 +0530

    metamask connect code

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,39 @@
+import abi from "./contracts/Chai.json";
+import { ethers } from "ethers";
+import { useState, useEffect } from "react";
+
 function App() {
+  const [state, setState] = useState({
+    provider: null,
+    signer: null,
+    contract: null,
+  });
+
+  useEffect(() => {
+    const connectWallet = async () => {
+      const contractAddress = "";
+      const contractABI = abi.abi;
+      try {
+        const { ethereum } = window;
+        if (ethereum) {
+          const account = await ethereum.request({
+            method: "eth_requestAccounts",
+          });
+        }
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const contract = new ethers.Contract(
+          contractAddress,
+          contractABI,
+          signer
+        );
+
+        setState({ provider, signer, contract });
+      } catch (error) {
+        console.log("Contract error is ", error);
+      }
+    };
+  }, []);
   return <h2>Hello world</h2>;
 }
 
